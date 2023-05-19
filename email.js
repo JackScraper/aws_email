ad536b254314302fb25e5148f1af7c1860125929
jackScraper <ivankrylov322@gmail.com>
2023-05-19 13:47:17 +0300

    email example with no html

---
+++ email.js
@@ -0,0 +1,46 @@
+import AWS from 'aws-sdk';
+
+const ses = new AWS.SES({ region: 'us-east-1' });
+
+export const handler = async (event) => {
+
+    // Hardcoded variables
+    const emailBody = 'bla-bla-bla' // Email message
+    const receiverEmail = 'hardwoodstefan@gmail.com'; // Receiver EMAIL
+    const emailTitle = `TEST EMAIL`; // Email title
+
+    const params = {
+        Destination: {
+            ToAddresses: [receiverEmail]
+        },
+        Message: {
+            Body: {
+                Text: {
+                    Data: emailBody
+                }
+            },
+            Subject: {
+                Data: emailTitle
+            }
+        },
+        Source: 'ivankrylov322@gmail.com' // email from which messages will be sent
+    };
+
+    try {
+        await ses.sendEmail(params).promise();
+
+        console.log('Message sent successfully');
+
+        return {
+            statusCode: 200,
+            body: 'Message sent successfully',
+        };
+    } catch (error) {
+        console.error('Error sending message:', error);
+
+        return {
+            statusCode: 500,
+            body: 'Error sending message',
+        };
+    }
+};
